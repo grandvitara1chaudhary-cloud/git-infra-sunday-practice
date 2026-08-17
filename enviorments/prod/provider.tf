a3bc75b396ec6586aae1ddb6ffcd63e27eb1cd25
grandvitara1chaudhary-cloud <grandvitara.1.chaudhary@gmail.com>
2026-08-17 22:16:06 +0530

    added backend block

--- enviorments/prod/provider.tf
+++ enviorments/prod/provider.tf
@@ -1,12 +1,20 @@
 terraform {
-    required_providers {
-      azurerm = {
-        source = "hashicorp/azurerm"
-        version = "5.0"
-
-      }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "5.0"
     }
+  }
+
+   backend "azurerm" {
+    resource_group_name  = "<rg-think>"
+    storage_account_name = "<rgthingstorageaccountgit>"
+    container_name       = "<gitpracticesgthink>"
+    key                  = "prod.terraform.tfstate"
+  }
 }
-provider "azurerm"{
-    features {}
+
+
+provider "azurerm" {
+  features {}
 }
