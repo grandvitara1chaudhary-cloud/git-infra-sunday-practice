274f90f7447191748ece0a93ea9b70380e552874
grandvitara1chaudhary-cloud <grandvitara.1.chaudhary@gmail.com>
2026-08-17 21:43:51 +0530

    added code

--- enviorments/preprod/main.tf
+++ enviorments/preprod/main.tf
@@ -5,6 +5,7 @@ module "resource_group" {
       
 }
 module "storage_account" {
+    depends_on = [ module.resource_group ]
     source = "../../modules/azurerm_storage_account"
     storage_accounts = var.storage_accounts
       
